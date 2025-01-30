f96db34a1d515f407ede362910f4ddf559f06557
Matias Kamelman <125071787+kamelmat@users.noreply.github.com>
2025-01-30 19:10:16 +0000

    Feature Matias Kamelman´s Excuse Generator on CLI with Python

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import random
-import math
+excuse = "My Cat Peed on my homework this morning"
 
-# your code here
+who = ["The Dog", "The Cat", "My Grandma", "My Sister", "An Alien"]
+what = ["ate", "peed", "broke", "stole", "took"]
+thing = ["my homework", "my snickers", "my meal", "my guitar", "my car"]
+when = ["this morning", "yesterday", "before coming here"]
+
+def generate_excuse():
+    return random.choice(who) + " " +random.choice(what) + " " + random.choice(thing) +  " " + random.choice(when)
+    
+    
+print(generate_excuse())
+
